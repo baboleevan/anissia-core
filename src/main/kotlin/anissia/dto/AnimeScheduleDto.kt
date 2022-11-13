0eb27afe4727ab514e6561779de4c98d94d57896
PARK Yong Seo <j@saro.me>
2022-11-13 23:05:08 +0900

    anime schedule

--- src/main/kotlin/anissia/dto/AnimeScheduleDto.kt
+++ src/main/kotlin/anissia/dto/AnimeScheduleDto.kt
@@ -4,9 +4,11 @@ import anissia.rdb.entity.Anime
 
 data class AnimeScheduleDto (
     var animeNo: Long = 0,
+    var week: String = "",
     var status: String = "",
     var time: String = "",
     var subject: String = "",
+    var originalSubject: String = "",
     var genres: String = "",
     var captionCount: Int = 0,
     var startDate: String = "",
@@ -14,10 +16,12 @@ data class AnimeScheduleDto (
     var website: String = "",
 ) {
     constructor(anime: Anime): this(
+        week = anime.week,
         animeNo = anime.animeNo,
         status = anime.status.toString(),
         time = if (!anime.week.matches("7|8".toRegex())) anime.time else anime.startDate,
         subject = anime.subject,
+        originalSubject = anime.originalSubject,
         genres = anime.genres,
         captionCount = anime.captionCount,
         startDate = anime.startDate,
